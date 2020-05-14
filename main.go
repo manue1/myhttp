3748b2bbb5edd5fb7edbc20b38cc7c3bca8abe62
manue1 <manuel.bergler@gmail.com>
2020-05-14 16:26:23 +0200

    Do requests and hash them sequentially

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 )
 
 func main() {
@@ -12,8 +17,54 @@ func main() {
 	log.Println("parallelCount: ", *parallelCount)
 	log.Println("urls: ", flag.Args())
 
-	// @ToDo in parallel:
-	// - make http request
-	// - hash the response body
-	// - print address hash (prepend http:// if not stated as part of arg)
+	for _, url := range flag.Args() {
+		sanitizedUrl := sanitizeProtocol(url)
+
+		resp, err := doRequest(sanitizedUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("%s %s", sanitizedUrl, computeHash(resp))
+	}
+}
+
+func doRequest(url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func sanitizeProtocol(url string) string {
+	var (
+		protocol  = "http://"
+		sanitized = url
+	)
+
+	if !strings.HasPrefix(url, protocol) {
+		sanitized = protocol + url
+	}
+
+	return sanitized
+}
+
+func computeHash(response []byte) string {
+	h := md5.New()
+	h.Write(response)
+
+	return hex.EncodeToString(h.Sum(nil))
 }
